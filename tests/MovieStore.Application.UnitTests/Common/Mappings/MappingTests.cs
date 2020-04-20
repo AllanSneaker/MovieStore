d6f643e004346c072d4001aff564503dbcf3a485
roman volyk <rovol@DESKTOP-358L1FL>
2020-04-21 00:00:17 +0300

    mapper tests passed

---
+++ tests/MovieStore.Application.UnitTests/Common/Mappings/MappingTests.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using MovieStore.Application.Common.Mappings;
+using MovieStore.Application.Common.Mappings.DTO;
+using MovieStore.Domain.Entities;
+using NUnit.Framework;
+using System;
+
+namespace MovieStore.Application.UnitTests.Common.Mappings
+{
+	public class MappingTests
+	{
+		private readonly IConfigurationProvider _configuration;
+		private readonly IMapper _mapper;
+
+		public MappingTests()
+		{
+			_configuration = new MapperConfiguration(cfg =>
+			{
+				cfg.AddProfile<MappingProfile>();
+			});
+
+			_mapper = _configuration.CreateMapper();
+		}
+
+		[Test]
+		public void ShouldHaveValidConfiguration()
+		{
+			_configuration.AssertConfigurationIsValid();
+		}
+
+		[Test]
+		[TestCase(typeof(Movie), typeof(MovieDto))]
+		public void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+		{
+			var instance = Activator.CreateInstance(source);
+
+			_mapper.Map(instance, source, destination);
+		}
+	}
+}
